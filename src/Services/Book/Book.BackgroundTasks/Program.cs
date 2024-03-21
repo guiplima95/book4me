7cec6802048dc72e96b70edae09e70ee695354c6
Guilherme Lima <guilhermedl@neowrk.com>
2024-03-21 16:55:54 -0300

    feat: creating sln and projects

---
+++ src/Services/Book/Book.BackgroundTasks/Program.cs
@@ -0,0 +1,7 @@
+using Book.BackgroundTasks;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ test/Book.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Book.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
